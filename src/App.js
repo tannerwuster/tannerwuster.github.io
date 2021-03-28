2138c4d887596363adff022f6ea5b6a6f46ee3a0
TannerWuster <tannerwuster@gmail.com>
2021-03-28 16:50:06 -0600

    app changed to using routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { AboutMe } from "./components/AboutMe";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header>
+        <Switch>
+          <Route exact={true} path="/"></Route>
+          <Route path="/src/components/AboutMe">
+            <AboutMe />
+          </Route>
+          {/* <Route path="/src/components/secondcomponent">
+            <AboutMe />
+          </Route>
+          <Route path="/src/components/thirdcomponent">
+            <AboutMe />
+          </Route> */}
+          {/* <Footer /> THIS IS WHERE IS SHOULD BE SO IT SHOWS UP ON EVERY PAGE*/}
+        </Switch>
+      </Header>
+      <Footer /> {/*TO WORK ON FOOTER INSERT HERE*/}
+    </Router>
   );
 }
 
