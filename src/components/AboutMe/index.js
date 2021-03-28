e8dc24849337f27a3dcb69e8f9324f9e6301f01d
TannerWuster <tannerwuster@gmail.com>
2021-03-28 16:50:34 -0600

    about me page

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { AboutMePage } from "./styles";
+
+export const AboutMe = () => {
+  return <AboutMePage></AboutMePage>;
+};
---
+++ src/components/AboutMe/styles.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const AboutMePage = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  transition: all 0.4s ease;
+`;
+
+// export const Image = styled.img`
+//   height: 35vh;
+//   margin-left: auto;
+//   margin-right: auto;
+//   display: block;
+// `;
+
+// export const Title = styled.h1`
+//   text-align: center;
+//   font-size: 56px;
+// `;
+
+// export const Label = styled.label`
+//   margin: 5px 0;
+//   height: 22px;
+// `;
+
+// export const ShowRegisterModalButton = styled.button.attrs({
+//   value: "Register",
+// })`
+//   text-align: center;
+//   position: relative;
+//   cursor: pointer;
+//   display: inline-block;
+//   border: none;
+//   color: #000000;
+//   font-weight: 300;
+//   font-size: 15px;
+//   background: transparent;
+
+//   &:hover {
+//     text-decoration: underline;
+//   }
+// `;
+
+// export const Tabs = styled.input`
+//   margin: 15px 0px;
+//   text-align: center;
+//   position: relative;
+//   text-decoration: none;
+//   margin-right: 20px;
+//   cursor: pointer;
+//   border: none;
+//   color: #000000;
+//   font-weight: 300;
+//   transition: all 0.3s ease;
+//   background: transparent;
+
+//   &:hover {
+//     text-decoration: underline;
+//   }
+// `;
