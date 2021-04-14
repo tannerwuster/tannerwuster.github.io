ec44c1fc44ecfbe4791272d7b0f8ea21a9348e11
tannerwuster <tannerwuster@gmail.com>
2021-04-13 21:06:49 -0600

    routes updated with three pages

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 
 import { Home } from "./components/Home";
 import { Contact } from "./components/Contact";
+import { Projects } from "./components/Projects";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
@@ -19,12 +20,11 @@ function App() {
     <Router>
       <Header />
       <Switch>
-        {/* <Route exact={true} path="/"></Route> */}
-        {/* <Route path="/src/components/home">
-            <Home />
-          </Route> */}
         <Route component={Home}>
           <Home />
+        </Route>
+        <Route component={Projects}>
+          <Projects />
         </Route>
         {/* <Route path="/src/components/Contact">
             <Contact />
