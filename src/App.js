b1393e848c13ebc389adfa5ec4d4efea8959bc81
tannerwuster <tannerwuster@gmail.com>
2021-04-19 10:37:11 -0600

    routes are still a WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
 import {
@@ -22,13 +21,14 @@ function App() {
       <Switch>
         <Route component={Home}>
           <Home />
-        </Route>
+        </Route>{" "}
+        */}
         <Route component={Projects}>
           <Projects />
         </Route>
-        {/* <Route path="/src/components/Contact">
-            <Contact />
-          </Route> */}
+        <Route component={Contact}>
+          <Contact />
+        </Route>
       </Switch>
       <Footer />
     </Router>
@@ -36,3 +36,42 @@ function App() {
 }
 
 export default App;
+
+// import "./App.css";
+// import React from "react";
+
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect,
+//   z,
+// } from "react-router-dom";
+
+// import { Home } from "./components/Home";
+// import { Contact } from "./components/Contact";
+// import { Projects } from "./components/Projects";
+// import { Header } from "./components/Header";
+// import { Footer } from "./components/Footer";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Header />
+//       <Switch>
+//         <Route path="./components/Home">
+//           <Home />
+//         </Route>
+//         <Route path="./components/Projects">
+//           <Projects />
+//         </Route>
+//         {/* <Route path="/src/components/Contact">
+//             <Contact />
+//           </Route> */}
+//       </Switch>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
