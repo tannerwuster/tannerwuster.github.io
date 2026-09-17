6547c34099a3dcaceee0010ebf3d88c69cbbd3b2
Tanner A. Wuster <tannerwuster@gmail.com>
2026-09-17 14:29:32 -0600

    Add TWU branding to site footer and favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,13 @@ export default function Footer() {
             d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
           />
         </svg>{" "}
-        by Tanner A. Wuster {new Date().getFullYear()}
+        by{" "}
+        <img
+          className="site-footer__maker-mark"
+          src="/twu-logo.png"
+          alt="Tanner A. Wuster"
+        />{" "}
+        {new Date().getFullYear()}
       </p>
     </footer>
   );
